86f2f99129ec9e6450f7a0cba4719682501bdf88
motiondiego <159647142+motiondiego@users.noreply.github.com>
2024-05-09 15:50:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+import {loadGLTF} from "./libs/loader.js";
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: './assets/targets/crab.mind',
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1.5 );
+    scene.add(light);
+
+    const gltf = await loadGLTF('./assets/models/Crab_A3.gltf');
+    gltf.scene.scale.set( 0.4, 0.4, 0.4);
+    gltf.scene.position.set(0, -0.2, 0);
+
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(gltf.scene);
+
+    const mixer = new THREE.AnimationMixer(gltf.scene);
+    const action = mixer.clipAction(gltf.animations[0]);
+    action.play();
+
+    const clock = new THREE.Clock();
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      const delta = clock.getDelta();
+      mixer.update(delta);
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
